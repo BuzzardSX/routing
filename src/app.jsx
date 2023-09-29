14b859e5576b07c97c4ec7db41088e3d6a82f60c
Павел Бобров <buzzard.sx@gmail.com>
2023-09-30 03:06:31 +0500

    suspense

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,7 @@
 import Router from './router'
+import { Suspense } from 'react'
 
-export default () => <Router />
+export default () =>
+	<Suspense fallback={<div>Loading...</div>}>
+		<Router />
+	</Suspense>
